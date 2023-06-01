dc494a9608f375f8106bf064a1aeffc1ecf8c0fa
Gorakapitiyage  Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2023-06-01 22:53:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 const jwt = require("jsonwebtoken");
-// var nodemailer = require("nodemailer");
 
 const JWT_SECRET =
   "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
@@ -104,90 +103,7 @@ app.listen(5000, () => {
   console.log("Server Started");
 });
 
-// app.post("/forgot-password", async (req, res) => {
-//   const { email } = req.body;
-//   try {
-//     const oldUser = await User.findOne({ email });
-//     if (!oldUser) {
-//       return res.json({ status: "User Not Exists!!" });
-//     }
-//     const secret = JWT_SECRET + oldUser.password;
-//     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-//       expiresIn: "5m",
-//     });
-//     const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
-//     var transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: "adarsh438tcsckandivali@gmail.com",
-//         pass: "rmdklolcsmswvyfw",
-//       },
-//     });
 
-//     var mailOptions = {
-//       from: "youremail@gmail.com",
-//       to: "thedebugarena@gmail.com",
-//       subject: "Password Reset",
-//       text: link,
-//     };
-
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log("Email sent: " + info.response);
-//       }
-//     });
-//     console.log(link);
-//   } catch (error) { }
-// });
-
-// app.get("/reset-password/:id/:token", async (req, res) => {
-//   const { id, token } = req.params;
-//   console.log(req.params);
-//   const oldUser = await User.findOne({ _id: id });
-//   if (!oldUser) {
-//     return res.json({ status: "User Not Exists!!" });
-//   }
-//   const secret = JWT_SECRET + oldUser.password;
-//   try {
-//     const verify = jwt.verify(token, secret);
-//     res.render("index", { email: verify.email, status: "Not Verified" });
-//   } catch (error) {
-//     console.log(error);
-//     res.send("Not Verified");
-//   }
-// });
-
-// app.post("/reset-password/:id/:token", async (req, res) => {
-//   const { id, token } = req.params;
-//   const { password } = req.body;
-
-//   const oldUser = await User.findOne({ _id: id });
-//   if (!oldUser) {
-//     return res.json({ status: "User Not Exists!!" });
-//   }
-//   const secret = JWT_SECRET + oldUser.password;
-//   try {
-//     const verify = jwt.verify(token, secret);
-//     const encryptedPassword = await bcrypt.hash(password, 10);
-//     await User.updateOne(
-//       {
-//         _id: id,
-//       },
-//       {
-//         $set: {
-//           password: encryptedPassword,
-//         },
-//       }
-//     );
-
-//     res.render("index", { email: verify.email, status: "verified" });
-//   } catch (error) {
-//     console.log(error);
-//     res.json({ status: "Something Went Wrong" });
-//   }
-// });
 
 app.get("/getAllUser", async (req, res) => {
   try {
@@ -211,51 +127,3 @@ app.post("/deleteUser", async (req, res) => {
 });
 
 
-// app.post("/upload-image", async (req, res) => {
-//   const { base64 } = req.body;
-//   try {
-//     await Images.create({ image: base64 });
-//     res.send({ Status: "ok" })
-
-//   } catch (error) {
-//     res.send({ Status: "error", data: error });
-
-//   }
-// })
-
-// app.get("/get-image", async (req, res) => {
-//   try {
-//     await Images.find({}).then(data => {
-//       res.send({ status: "ok", data: data })
-//     })
-
-//   } catch (error) {
-
-//   }
-// })
-
-// app.get("/paginatedUsers", async (req, res) => {
-//   const allUser = await User.find({});
-//   const page = parseInt(req.query.page)
-//   const limit = parseInt(req.query.limit)
-
-//   const startIndex = (page - 1) * limit
-//   const lastIndex = (page) * limit
-
-//   const results = {}
-//   results.totalUser=allUser.length;
-//   results.pageCount=Math.ceil(allUser.length/limit);
-
-//   if (lastIndex < allUser.length) {
-//     results.next = {
-//       page: page + 1,
-//     }
-//   }
-//   if (startIndex > 0) {
-//     results.prev = {
-//       page: page - 1,
-//     }
-//   }
-//   results.result = allUser.slice(startIndex, lastIndex);
-//   res.json(results)
-// })
